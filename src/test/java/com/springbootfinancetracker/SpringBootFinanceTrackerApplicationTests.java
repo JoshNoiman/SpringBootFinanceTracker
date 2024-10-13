14a960f5d8f3e54d29bf29950d9d7b22c6bb0e15
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-12 23:27:47 -0400

    Added necessary methods needed for test for income/expenses test

--- src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
+++ src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
@@ -12,7 +12,9 @@ class SpringBootFinanceTrackerApplicationTests {
 
     @Test
     void addIncomeAndSubtractExpensesToGetNewValue() {
-
+        getUserIncome();
+        addUserIncome();
+        calculateUserBalance();
     }
 
 }
