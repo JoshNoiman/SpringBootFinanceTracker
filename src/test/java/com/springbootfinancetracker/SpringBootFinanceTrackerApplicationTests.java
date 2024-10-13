448b992a6ccd79b678b8d569b4f3f6f613d35492
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-12 23:22:47 -0400

    Adjusted Test name to be more clear

--- src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
+++ src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
@@ -11,7 +11,7 @@ class SpringBootFinanceTrackerApplicationTests {
     }
 
     @Test
-    void addIncomeAndExpensesToGetNewValue() {
+    void addIncomeAndSubtractExpensesToGetNewValue() {
 
     }
 
