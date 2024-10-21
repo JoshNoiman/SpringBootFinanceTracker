cc8e4a0c86cee1d67833fc8a601708601db1b121
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-20 23:41:58 -0400

    Removed testing code into new branch

--- src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
+++ src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
@@ -13,28 +13,4 @@ class SpringBootFinanceTrackerApplicationTests {
     void contextLoads() {
     }
 
-    @Test
-    void addIncomeAndSubtractExpensesToGetNewValue() {
-        getUserIncome();
-        addUserIncome();
-        calculateUserBalance();
-        returnUsersNewBalance();
-    }
-    private void getUserIncome() {
-        account = user.userId();
-    }
-    private void addUserIncome() {
-        account.getIncome = account.getOldIncome() + account.getNewIncome();
-    }
-    private void calculateUserBalance() {
-        account.balance = account.getIncome - account.expenses();
-    }
-    private void returnUsersNewBalance() {
-        double newBalance = account.balance();
-        double expectedBalance = 30.00;
-        double delta = 0.001;
-        assertEquals(expectedBalance, newBalance, delta);
-    }
-
-
 }
