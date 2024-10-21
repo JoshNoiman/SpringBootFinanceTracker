a5a5c2048d1be2dec77e8a58086a771948900d7c
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-20 23:44:28 -0400

    Initial test code moved from interface_dev branch

--- src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
+++ src/test/java/com/springbootfinancetracker/SpringBootFinanceTrackerApplicationTests.java
@@ -3,11 +3,37 @@ package com.springbootfinancetracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringBootFinanceTrackerApplicationTests {
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void addIncomeAndSubtractExpensesToGetNewValue() {
+        getUserIncome();
+        addUserIncome();
+        calculateUserBalance();
+        returnUsersNewBalance();
+    }
+    private void getUserIncome() {
+        account = user.userId(1);
+    }
+    private void addUserIncome() {
+        account.getIncome = account.getOldIncome() + account.getNewIncome();
+    }
+    private void calculateUserBalance() {
+        account.balance = account.getIncome - account.expenses();
+    }
+    private void returnUsersNewBalance() {
+        double newBalance = account.balance();
+        double expectedBalance = 30.00;
+        double delta = 0.001;
+        assertEquals(expectedBalance, newBalance, delta);
+    }
+
 
 }
