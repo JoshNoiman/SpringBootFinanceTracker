72c74c846998495ac2ca29e8c723c59a09f7d258
Nathan Burns <nate10nate@gmail.com>
2024-10-24 10:58:47 -0400

    added javadoc comments

--- src/main/java/com/springbootfinancetracker/controller/TransactionController.java
+++ src/main/java/com/springbootfinancetracker/controller/TransactionController.java
@@ -10,14 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.springbootfinancetracker.dto.TransactionDto;
 
+/**
+ * Controller for handling transaction-related requests.
+ */
 @RestController
 public class TransactionController {
 
+    /**
+     * Fetches all transactions.
+     * @return the list of all transactions
+     */
     @GetMapping("/transaction")
     public List<TransactionDto> getAllTransactions() {
         return Arrays.asList(new TransactionDto());
     }
 
+    /**
+     * Creates a new transaction.
+     * @param transaction the transaction DTO
+     * @return the created transaction DTO
+     */
     @PostMapping("/transaction")
     public TransactionDto createTransaction(@RequestBody TransactionDto transaction) {
         System.out.println(transaction);
