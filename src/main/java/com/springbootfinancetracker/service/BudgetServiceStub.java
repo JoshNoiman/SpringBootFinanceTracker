6ae148ff18ba61c2b6a555a310f82fbaa134a609
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-26 13:43:27 -0400

    Added hardcoded logic for updateBudget()

--- src/main/java/com/springbootfinancetracker/service/BudgetServiceStub.java
+++ src/main/java/com/springbootfinancetracker/service/BudgetServiceStub.java
@@ -43,7 +43,7 @@ public class BudgetServiceStub implements IBudgetService {
      */
     @Override
     public BudgetDto updateBudget(BudgetDto budget) throws Exception {
-        return null;
+        return budgetDao.updateBudget(budget);
     }
 
     /**
