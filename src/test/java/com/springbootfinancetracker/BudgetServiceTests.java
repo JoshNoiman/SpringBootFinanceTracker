8ae711393ac73ebb4f6491d211be3e50320aa9d4
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-26 16:07:10 -0400

    Create initial test file for BudgetServiceTests with annotations and imports

---
+++ src/test/java/com/springbootfinancetracker/BudgetServiceTests.java
@@ -0,0 +1,25 @@
+package com.springbootfinancetracker;
+
+import jakarta.annotation.ManagedBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Scope;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ManagedBean
+@Scope("session")
+public class BudgetServiceTests {
+
+}
