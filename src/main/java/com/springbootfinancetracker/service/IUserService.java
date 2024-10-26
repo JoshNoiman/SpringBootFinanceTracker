ae022ba765187f0ea44ca2743f6361b8a1248482
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-26 12:57:05 -0400

    Added fetchAllUsers and removed uneccessary public prefix from other boilerplate methods

--- src/main/java/com/springbootfinancetracker/service/IUserService.java
+++ src/main/java/com/springbootfinancetracker/service/IUserService.java
@@ -2,6 +2,8 @@ package com.springbootfinancetracker.service;
 
 import com.springbootfinancetracker.dto.UserDto;
 
+import java.util.List;
+
 public interface IUserService {
     
     /**
@@ -10,14 +12,21 @@ public interface IUserService {
      * @return User
      * @throws Exception
      */
-    public UserDto saveUser(UserDto newUser) throws Exception;
+    UserDto saveUser(UserDto newUser) throws Exception;
 
     /**
      * 
      * @param id
      * @return User id
      */
-    public UserDto fetchById(int id);
+    UserDto fetchById(int id) throws Exception;
+
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
+    List<UserDto> fetchAllUsers() throws Exception;
 
 
 }
