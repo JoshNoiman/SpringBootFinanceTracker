059ad09f089bb2219f8c1807bad9d868e1e6c979
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-26 13:54:27 -0400

    Add annotations and initial test method

--- src/test/java/com/springbootfinancetracker/UserServiceTests.java
+++ src/test/java/com/springbootfinancetracker/UserServiceTests.java
@@ -1,9 +1,34 @@
 package com.springbootfinancetracker;
 
-import org.junit.jupiter.api.Test;
+import com.springbootfinancetracker.dao.IUserDao;
+import com.springbootfinancetracker.service.IUserService;
+import com.springbootfinancetracker.service.UserServiceStub;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@Scope("session")
 public class UserServiceTests {
+
+    @Autowired
+    @InjectMocks
+    private UserServiceStub userService;
+
+    @MockBean
+    private IUserDao userDao;
+
+    @Test
+    public void testFetchById() {
+
+    }
+
+
 
 }
