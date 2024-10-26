d1034bb88a01fec96bd2144af45abd9f66c85707
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-26 12:06:14 -0400

    Add initial code for BudgetDaoStub

---
+++ src/main/java/com/springbootfinancetracker/dao/BudgetDaoStub.java
@@ -0,0 +1,47 @@
+package com.springbootfinancetracker.dao;
+
+import com.springbootfinancetracker.dto.BudgetDto;
+
+import java.util.List;
+
+public class BudgetDaoStub implements IBudgetDao {
+
+    /**
+     * @param budgetDto
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public BudgetDto createBudget(BudgetDto budgetDto) throws Exception {
+        return null;
+    }
+
+    /**
+     * @param budgetDto
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public BudgetDto updateBudget(BudgetDto budgetDto) throws Exception {
+        return null;
+    }
+
+    /**
+     * @param id
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public BudgetDto getBudgetById(int id) throws Exception {
+        return null;
+    }
+
+    /**
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<BudgetDto> getAllBudgets() throws Exception {
+        return List.of();
+    }
+}
