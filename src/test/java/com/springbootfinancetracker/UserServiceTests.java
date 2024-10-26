99970d6ba12726c4999a885f1a629b1c0ec8fe60
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-10-26 14:25:44 -0400

    Set initial test through use of Arrange, Act, and Assert best practices. Test should pass.

--- src/test/java/com/springbootfinancetracker/UserServiceTests.java
+++ src/test/java/com/springbootfinancetracker/UserServiceTests.java
@@ -2,16 +2,15 @@ package com.springbootfinancetracker;
 
 import com.springbootfinancetracker.dao.IUserDao;
 import com.springbootfinancetracker.dto.UserDto;
-import com.springbootfinancetracker.service.IUserService;
 import com.springbootfinancetracker.service.UserServiceStub;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,13 +24,19 @@ public class UserServiceTests {
     private IUserDao userDao;
 
     @Test
-    public void testFetchByUserIdAndReturnFirstId() {
+    public void testFetchByUserIdAndReturnFirstUserIdContents() {
         // Arrange
         int userId = 1;
 
         // Act
+        UserDto user = userService.fetchById(userId);
 
         // Assert
+        assertNotNull(user);
+        assertEquals(Integer.valueOf(1), user.getUserId());
+        assertEquals("Joe Brave", user.getUsername());
+        assertEquals("Pa$$w0rd", user.getPassword());
+
     }
 
 
