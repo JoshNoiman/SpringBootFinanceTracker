5faef52afa694fb8f1b45d6407f0bbcf554b18f0
grisbyar <grisbyar@mail.uc.edu>
2024-11-03 17:24:04 -0500

    Commit 1: BudgetController changes, using ResponseEntity

--- src/main/java/com/springbootfinancetracker/controller/BudgetController.java
+++ src/main/java/com/springbootfinancetracker/controller/BudgetController.java
@@ -1,12 +1,12 @@
 package com.springbootfinancetracker.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 import com.springbootfinancetracker.dto.BudgetDto;
 
@@ -14,13 +14,15 @@ import com.springbootfinancetracker.dto.BudgetDto;
 public class BudgetController{
 
     @GetMapping("/budget")
-    public List<BudgetDto> getAllBudgets() {
-        return Arrays.asList(new BudgetDto());
+    public ResponseEntity<List<BudgetDto>> getAllBudgets() {
+        List<BudgetDto> budgets = List.of(new BudgetDto());
+        return ResponseEntity.ok(budgets);
     }
 
     @PostMapping("/budget")
-    public BudgetDto createBudgetDto(@RequestBody BudgetDto budget) {
+    public ResponseEntity<BudgetDto> createBudgetDto(@RequestBody BudgetDto budget) {
         System.out.println(budget);
-        return budget;
+        return ResponseEntity.status(201).body(budget);
     }
+
 }
