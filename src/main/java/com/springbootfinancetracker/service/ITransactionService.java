d885ecd5268093e1d52c259707b2b6e1eb0065e0
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-11-11 16:29:00 -0500

    Added javadoc and added map for viewing totals of each category in a map.

--- src/main/java/com/springbootfinancetracker/service/ITransactionService.java
+++ src/main/java/com/springbootfinancetracker/service/ITransactionService.java
@@ -4,48 +4,62 @@ import com.springbootfinancetracker.dto.TransactionDto;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public interface ITransactionService {
 
     /**
+     * Adds income to transaction with specified category & amount.
      *
-     * @param amount
-     * @param category
-     * @return
+     * @param amount The amount of income
+     * @param category Category associated with amount
+     * @return new income
      */
     TransactionDto addIncomeTransaction(double amount, String category);
 
     /**
+     * Adds expense to transaction with specified category & amount.
      *
-     * @param amount
-     * @param category
-     * @return
+     * @param amount The amount of expense to subtract
+     * @param category Category expense will be applied to
+     * @return new expense
      */
     TransactionDto addExpenseTransaction(double amount, String category);
 
     /**
+     * Grabs total income associated with transaction.
      *
-     * @return
+     * @return transaction's total income
      */
     double getTotalIncome();
 
     /**
+     * Grabs total expense for transaction
      *
-     * @return
+     * @return transactions total expense
      */
     double getTotalExpense();
 
     /**
+     * Get total amount after income & expense calculation for transaction in a specific category
      *
-     * @param category
-     * @return
+     * @param category category to grab income & expense calculated value
+     * @return total of category after income & expense
      */
     double getTotalByCategory(String category);
 
 
     /**
+     * Retrieves all currently implemented categories specified.
      *
-     * @return
+     * @return all categories
      */
     List<String> allCategories = new ArrayList<>();
+
+    /**
+     * Grabs totals for each of the categories.
+     *
+     * @return a map of category names with their associated total amounts
+     */
+    Map<String, Double> getTotalsByCategory();
 }
