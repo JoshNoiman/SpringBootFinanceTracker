26dc5d0e6b67960e8ce22c69a7c31d6da57282e9
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2024-11-11 16:07:12 -0500

    Changed around interface method contract names. Added javadoc

--- src/main/java/com/springbootfinancetracker/service/ITransactionService.java
+++ src/main/java/com/springbootfinancetracker/service/ITransactionService.java
@@ -2,13 +2,50 @@ package com.springbootfinancetracker.service;
 
 import com.springbootfinancetracker.dto.TransactionDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface ITransactionService {
-    TransactionDto addTransaction(TransactionDto transactionDto);
-    List<TransactionDto> getAllTransactions();
-    TransactionDto getTransactionById(int id);
-    TransactionDto updateTransaction(int id, TransactionDto transactionDto);
-    void deleteTransaction(int id);
-    List<String> allCategories = List.of();
+
+    /**
+     *
+     * @param amount
+     * @param category
+     * @return
+     */
+    TransactionDto addIncomeTransaction(double amount, String category);
+
+    /**
+     *
+     * @param amount
+     * @param category
+     * @return
+     */
+    TransactionDto addExpenseTransaction(double amount, String category);
+
+    /**
+     *
+     * @return
+     */
+    double getTotalIncome();
+
+    /**
+     *
+     * @return
+     */
+    double getTotalExpense();
+
+    /**
+     *
+     * @param category
+     * @return
+     */
+    double getTotalByCategory(String category);
+
+
+    /**
+     *
+     * @return
+     */
+    List<String> allCategories = new ArrayList<>();
 }
